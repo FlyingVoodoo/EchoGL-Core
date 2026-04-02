a26b993abc32367960790705fd076bba14a20fb2
Ефимовых Матвей <motysha1905@gmail.com>
2026-04-02 12:33:08 +0300

    Add .gitkeep files for asset, docs, scripts, tests, and third_party directories; implement addGame method in DBManager

--- src/data/DBManager.cpp
+++ src/data/DBManager.cpp
@@ -23,3 +23,41 @@ void DBManager::execute(const std::string& sql) {
         throw DatabaseException(error, rc);
     }
 }
+
+void DBManager::addGame(const Game& game) {
+    sqlite3_stmt* raw_stmt = nullptr;
+    int rc = sqlite3_prepare_v2(
+        m_db.get(),
+        "INSERT INTO games (title, description, cover_path, "
+        "steam_playtime_seconds, steam_synced_at) "
+        "VALUES (?, ?, ?, ?, ?)",
+        -1, &raw_stmt, nullptr
+    );
+    StatementHandle stmt(raw_stmt);
+    if (rc != SQLITE_OK) {
+        throw DatabaseException(sqlite3_errmsg(m_db.get()), rc);
+    }
+
+    sqlite3_bind_text(stmt.get(), 1, game.title.c_str(), -1, SQLITE_TRANSIENT);
+
+    if (game.description)
+        sqlite3_bind_text(stmt.get(), 2, game.description->c_str(), -1, SQLITE_TRANSIENT);
+    else
+        sqlite3_bind_null(stmt.get(), 2);
+
+    if (game.cover_path)
+        sqlite3_bind_text(stmt.get(), 3, game.cover_path->c_str(), -1, SQLITE_TRANSIENT);
+    else
+        sqlite3_bind_null(stmt.get(), 3);
+
+    sqlite3_bind_int(stmt.get(), 4, game.steam_playtime_seconds);
+
+    if (game.steam_synced_at)
+        sqlite3_bind_int(stmt.get(), 5, *game.steam_synced_at);
+    else
+        sqlite3_bind_null(stmt.get(), 5);
+
+    if (sqlite3_step(stmt.get()) != SQLITE_DONE) {
+        throw DatabaseException(sqlite3_errmsg(m_db.get()), SQLITE_ERROR);
+    }
+}
--- src/data/DBManager.hpp
+++ src/data/DBManager.hpp
@@ -15,12 +15,12 @@ public:
     explicit DBManager(const std::string& dbPath, const std::string& schema);
     ~DBManager();
 
-    bool addGame(const Game& game);
+    void addGame(const Game& game);
     std::optional<Game> getGameById(int id);
     std::vector<Game> getAllGames();
-    bool updateGameMetadata(int id, const std::string& description, 
+    void updateGameMetadata(int id, const std::string& description, 
                             const std::string& coverPath);
 
     std::optional<int> startSession(int gameId);
-    bool endSession(int sessionId);
+    void endSession(int sessionId);
 };
